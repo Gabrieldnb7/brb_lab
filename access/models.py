f3c0b123b52148a541230e268d6011dfcca374df
César <borges.lucascs@gmail.com>
2025-05-01 00:47:48 -0300

    criando model access

--- access/models.py
+++ access/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import Usuario
 
-# Create your models here.
+class Acesso(models.Model):
+    idAcesso = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario,db_column='id_usuario',on_delete=models.CASCADE)
+    Ambiente = models.CharField(max_length=45)
+    criadoEm = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'acesso'
+
+    def _str_(self):
+        return f"{self.usuario.Nome} - {self.Ambiente}"
