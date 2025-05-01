ee00de539381bb3136d4bb988584cfd4cd53a475
Cotonete97 <gabrielcots97@hotmail.com>
2025-04-30 22:33:24 -0300

    Criaçãoda model event

--- event/models.py
+++ event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Eventos(models.Model):
+    id_usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=45)
+    data = models.DateField()
+    horario = models.CharField(max_length=45)
+    ambiente = models.CharField(max_length=45)
+    descricao = models.CharField(max_length=500)
+    palestrantes = models.CharField(max_length=45)
+    checkin_code = models.CharField(max_length=45, null=True, blank=True)
+    status = models.CharField(max_length=45)
+    total_inscritos = models.IntegerField()
+    criado_em = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
