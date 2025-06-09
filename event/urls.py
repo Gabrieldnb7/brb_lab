b3ad6ef42c2085362796ec98ba2be1b2eb5e7009
ocanibill <biancocosta01@gmail.com>
2025-06-09 17:08:32 -0300

    fix: corrigindo urls para o novo nome de eventos

--- event/urls.py
+++ event/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from event.views import events, myEvents, registerUser, unsubscribe, eventDescription
+from event.views import events, myEvents, registerUser, unsubscribe, eventsDescription
 
 app_name = 'event'
 
 urlpatterns = [
     path('eventos', events, name='eventos'),
     path('meusEventos', myEvents, name='meusEventos'),
-    path('eventos/descricao/<int:id_evento>', eventDescription, name='descricao'), 
+    path('eventos/descricao/<int:id_evento>', eventsDescription, name='descricao'), 
     path('eventos/inscrever/<int:id_evento>', registerUser, name='inscrever'),
     path("eventos/cancelar/<int:id_evento>", unsubscribe, name="remover_inscricao"),
 ]
