52bda2e9ec1e2a567021cb06876e01f086faedbd
ocanibill <biancocosta01@gmail.com>
2025-06-10 10:39:49 -0300

    fix: correção de regra para envio de novos eventos

--- event/signals.py
+++ event/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timezone import now
 from .models import Eventos, Inscricao
 
 @receiver(post_save, sender=Eventos)
-def expire_subscriptions_from_finished_events(send, instance, **kwargs):
-    if instance.status == 0:
+def expire_subscriptions_from_finished_events(sender, instance, **kwargs):
+    if instance.data < now().date():  # Verifica se a data do evento já passou
         Inscricao.objects.filter(id_evento=instance.id, status=0).update(status=2)
